4cb60129108f08c3c15ef8a91492cc9aaedec37a
gabriel-iac <gabriele.iacopetti@gmail.com>
2018-06-10 19:00:16 +0100

    intialized rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  concern api_base do
-    
+  concern :api_base do
+
   end
 
   namespace :v1 do
